c209d1e0976b400f97f8fb90d372f7f691e6ec5b
flandy12 <halloflandy@gmail.com>
2020-08-09 20:00:17 +0700

    Kompomen Js #1

---
+++ Js/mainJS.js
@@ -0,0 +1,37 @@
+// class ListFilm extends HTMLElement {
+//     set List(artikelx) {
+//         this.artikelx_ = artikelx;
+//         this.rander();
+//     }
+//     rander() {
+//         this.artikelx_.forEach(Item => {
+//             const Element = document.createElement("article-item");
+//             Element.Item = Item;
+//             this.appendChild(Element);
+//         });
+//     }
+// }
+
+// class ItemAll extends HTMLElement {
+//     set Item(Brand) {
+//         this.brand_ = Brand;
+//         this.rander();
+//     }
+//     rander() {
+//         this.innerHTML = ``
+//     }
+// }
+
+// const user = {
+//     enemy : "lord"
+// }
+// let {enemy} = user;
+// enemy = "landy"
+// console.log(enemy);
+
+ShadowRoot.innerHtml += `
+<style>
+    .main-one{
+        background:white;
+    }
+</style>`
