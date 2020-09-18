69decc6fa08f2e332a649de8d0914af5d213fcb0
flandy12 <halloflandy@gmail.com>
2020-09-18 21:39:14 +0700

    file js add class

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+
 //sweetAlert action
-window.setTimeout(() => {
-    swal({
-        text: 'Flandy ',
-        title: "Welcome My Design",
-        button: "OKe",
-        icon: `./img/icon.png`
-    });
-}, 3000);
+// window.setTimeout(() => {
+//     swal({
+//         text: 'Flandy ',
+//         title: "Welcome My Design",
+//         button: "OKe",
+//         icon: `./img/icon.png`
+//     });
+// }, 3000);
+// setTimeout(() => {
+//     window.open('http://localhost:8080/login.html');
+// }, 3000);
+
+
+
 
 // const displayTime = () => {
 //     moment.locale("id");
@@ -51,13 +58,16 @@ $(document).ready(function () {
             active.className = 'item-list';
         })
         b.target.classList.add('active-img');
+        console.log(img_item.getAttribute('src'));
     })
 });
-
-
-
 
 // img_container.addEventListener('click', function (b) {
 
 //     // alert('ahlo');
 // })
+
+const login = document.querySelector(".login-main");
+login.addEventListener("click",function(){
+    window.open("http://localhost/DOM3/signin_.php");
+});
